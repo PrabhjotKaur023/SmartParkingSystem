a5be246515df53508146a59010ace852d9c4e732
PrabhjotKaur023 <kaur.prabhjot2489@gmail.com>
2026-06-03 20:11:38 +0530

    Create dashboard UI and integrate dynamic parking slot statistics
    Implemented the initial dashboard for the Smart Parking Management System.
    
    - Added dashboard page using Bootstrap
    - Configured URL routing and views
    - Connected dashboard to Django templates
    - Integrated dynamic parking slot statistics from the database
    - Displayed total, available, and occupied parking slots

--- parking/views.py
+++ parking/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import ParkingSlot
 
-# Create your views here.
+
+def dashboard(request):
+
+    total_slots = ParkingSlot.objects.count()
+
+    available_slots = ParkingSlot.objects.filter(
+        is_occupied=False
+    ).count()
+
+    occupied_slots = ParkingSlot.objects.filter(
+        is_occupied=True
+    ).count()
+
+    context = {
+        'total_slots': total_slots,
+        'available_slots': available_slots,
+        'occupied_slots': occupied_slots
+    }
+
+    return render(
+        request,
+        'parking/dashboard.html',
+        context
+    )
